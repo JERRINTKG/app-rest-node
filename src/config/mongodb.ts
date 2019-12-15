1fefc1c3664dfa782914fa42ef5a403b282432b3
jerrintkg <jerin.t@twinsway.in>
2019-12-15 14:02:26 +0530

    db test in  progress.

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,29 +1,29 @@
-import { MongoClient } from "mongodb";
+import * as mongoose from "mongoose";
 import globalConstants from "./globalConstants";
 
-export class MongodbConnect {
-  private connectionString;
-  public client;
+export default class mongodbConnect {
+  connectionString=globalConstants.moongodb
   constructor() {
-      this.connectionString = globalConstants.moongodb;
+    this.setupDb();
   }
 
-  async connect() {
-    // add async
-    console.log("connecting to mongo");
+  private setupDb(): void {
+    var UserSchema = new mongoose.Schema({
+      username: String,
+      email: String,
+    }, {timestamps: true,collection: 'user' },);
+    
+    let user = mongoose.model('user', UserSchema);
 
-    try {
-      if (!this.client) {
-        // I added this extra check
-        console.log("setting client");
-        this.client = await MongoClient.connect(this.connectionString, {
-          useNewUrlParser: true
-        });
-        console.log(this.client);
-      }
-    } catch (error) {
-      console.log("error during connecting to mongo: ");
-      console.error(error);
-    }
+    mongoose.connect(this.connectionString,{ useNewUrlParser: true,useUnifiedTopology: true });
+    var db = mongoose.connection;
+    console.log(mongoose.connection.readyState);
+    user.find((err,doc)=>{
+      console.log(doc);
+      
+    })
+    db.on("error", console.error.bind(console, "MongoDB Connection error"));
   }
+
 }
+  
