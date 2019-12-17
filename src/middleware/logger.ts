cb35f7a7c0610e0ecc06396d946a489186539685
jerrintkg <jerin.t@twinsway.in>
2019-12-17 22:56:55 +0530

    logger added.

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,19 @@
-import { Request, Response } from 'express'
+import { Request, Response } from "express";
+import * as mongoose from "mongoose";
+import { LoggerSchema } from "../schema/logger/Logger.schema";
 
 const loggerMiddleware = (req: Request, resp: Response, next) => {
-    // console.log('Request logged:', req.method, req.path)
-    next()
-}
+  // console.log('Request logged:', req.method, req.path)
+  let loggerModel = mongoose.model("log", LoggerSchema);
 
-export default loggerMiddleware
+  loggerModel.create(
+    { route: req.path, method: req.method, dateTime: new Date() },
+    function(err, small) {
+      if (err) return console.log(err);
+      console.log("Success");
+    }
+  );
+  next();
+};
+
+export default loggerMiddleware;
---
+++ src/schema/logger/Logger.schema.ts
@@ -0,0 +1,11 @@
+import * as mongoose from "mongoose";
+
+var LoggerSchema = new mongoose.Schema(
+  {
+    route: String,
+    method:String,
+    dateTime: { type: Date, default: Date.now },
+  }
+);
+
+export { LoggerSchema };
