d2b9fb68e343f85c5f8dbf0f8aee1969ef570ccb
jerrintkg <jerin.t@twinsway.in>
2019-12-18 22:26:02 +0530

    update

--- src/api/user/User.route.ts
+++ src/api/user/User.route.ts
@@ -1,12 +1,16 @@
 import * as express from "express";
 import UserController from "../../controller/user/User.controller";
+import  Common from "../../helper/common";
 
 let router = express.Router();
 
+// let helper = new Common();
 let data = new UserController();
 
+// let path = helper.getPathUrl(__dirname);
+
 let UserRoutes = {
-  all: router.get("/all",  (req, res) => {
+  all: router.get("api/user/all",  (req, res) => {
     data.getAllUsers(req,res);
   }),
   meta: router.get("/meta", (req, res) => {
---
+++ src/helper/common.ts
@@ -0,0 +1,10 @@
+import { basename } from "path";
+
+export default class Common {
+  constructor() {}
+
+//   getPathUrl(url) {
+//     let path = url.split("/");
+//     path.filter(m=>m==)
+//   }
+}
